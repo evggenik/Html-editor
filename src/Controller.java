7a2d8435c63a2cfdf8b60b5130ae18ec7d825c13
Evgeny Buchelnikov <gru554@yandex.ru>
2026-01-11 19:10:04 +1000

    created Controller and View

---
+++ src/Controller.java
@@ -0,0 +1,17 @@
+import javax.swing.text.html.HTMLDocument;
+import java.io.File;
+
+public class Controller {
+
+    private View view;
+    private HTMLDocument document;
+    private File currentFile;
+
+    public Controller(View view) {
+        this.view = view;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
