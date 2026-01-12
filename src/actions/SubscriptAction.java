13d91de47202c8edace652ec516b28c04cda8003
Evgeny Buchelnikov <gru554@yandex.ru>
2026-01-12 16:15:08 +1000

    added actions package

---
+++ src/actions/SubscriptAction.java
@@ -0,0 +1,16 @@
+package actions;
+
+import javax.swing.text.StyledEditorKit;
+import java.awt.event.ActionEvent;
+
+public class SubscriptAction extends StyledEditorKit.StyledTextAction {
+
+    public SubscriptAction() {
+        super(null);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
---
+++ src/listeners/UndoMenuListener.java
@@ -0,0 +1,27 @@
+package listeners;
+
+import view.View;
+
+import javax.swing.*;
+import javax.swing.event.MenuEvent;
+import javax.swing.event.MenuListener;
+
+public class UndoMenuListener implements MenuListener {
+    public UndoMenuListener(View view, JMenuItem undoMenuItem, JMenuItem redoMenuItem) {
+    }
+
+    @Override
+    public void menuSelected(MenuEvent e) {
+
+    }
+
+    @Override
+    public void menuDeselected(MenuEvent e) {
+
+    }
+
+    @Override
+    public void menuCanceled(MenuEvent e) {
+
+    }
+}
